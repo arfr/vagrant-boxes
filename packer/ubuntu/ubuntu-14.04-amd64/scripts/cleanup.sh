6b235db3bf4bcb3ff93e3a5b92d8bf0cd0d29f6e
Alexander Fuhr <m@arfr.de>
2015-08-14 23:16:08 +0200

    2:update cleanup.sh: remove inserv from cleanup

--- packer/ubuntu/ubuntu-14.04-amd64/scripts/cleanup.sh
+++ packer/ubuntu/ubuntu-14.04-amd64/scripts/cleanup.sh
@@ -13,30 +13,6 @@ mkdir /etc/udev/rules.d/70-persistent-net.rules
 
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
-cat << 'EOF' > /etc/init.d/ssh_gen_host_keys
-#!/bin/sh
-### BEGIN INIT INFO
-# Provides: Generates new ssh host keys on first boot
-# Required-Start: $remote_fs $syslog
-# Required-Stop: $remote_fs $syslog
-# Default-Start: 2 3 4 5
-# Default-Stop:
-# Short-Description: Generates new ssh host keys on first boot
-# Description: Generates new ssh host keys on first boot
-### END INIT INFO
-rm -f /etc/ssh/ssh_host_*
-
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -t rsa -N ""
-ssh-keygen -f /etc/ssh/ssh_host_dsa_key -t dsa -N ""
-insserv -r /etc/init.d/ssh_gen_host_keys
-
-rm -f /etc/init.d/ssh_gen_host_keys
-EOF
-
-chmod a+x /etc/init.d/ssh_gen_host_keys
-ln -s /usr/lib/insserv/insserv /sbin/insserv
-insserv /etc/init.d/ssh_gen_host_keys
-
 rm -rf /tmp/*
 
 rm -rf /usr/src/virtualbox-ose-guest*
